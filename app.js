7376d3e759ae338c57f13e19be2621d5be07884c
aoigj100a <aoigj100@gmail.com>
2021-02-16 17:28:07 +0800

    feat:新增bodyParser取得表單資料

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
@@ -9,6 +10,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 mongoose.set('useCreateIndex', true)
 mongoose.connect('mongodb://localhost/URL', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -21,6 +24,10 @@ db.once('open', () => {
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+app.post('/',(req, res)=>{
+    console.log(req.body)
+    res.send('收表單囉')
 })
 
 app.listen(port, () => {
