5fb7e169aa4b80fc300544217f688ed25955c1b6
aoigj100a <aoigj100@gmail.com>
2021-02-16 16:20:47 +0800

    feat:新增mongoose 連線db

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
 
-app.engine('hbs',exphbs({defaultLayout:'main', extname: '.hbs'}))
-app.set('view engine' , 'hbs')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+mongoose.set('useCreateIndex', true)
+mongoose.connect('mongodb://localhost/URL', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+    console.log('mongoDB error!')
+})
+db.once('open', () => {
+    console.log('mongoDB connected!')
+})
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log(`已經連線到http://localhost:${port}`)
 })
